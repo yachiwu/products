9dffe445ef01f50cad7a25abdc96498abc46b722
Lucy <n0975116268@gmail.com>
2018-12-19 19:08:08 +0800

    can write

--- products.py
+++ products.py
@@ -12,3 +12,7 @@ print(products)
 #印出全部商品
 for product in products:
 	print(product[0],'的價格是',product[1])
+
+with open ('products.csv','w') as f:
+	for product in products:
+		f.write(product[0] + ',' + product[1] + '\n')
